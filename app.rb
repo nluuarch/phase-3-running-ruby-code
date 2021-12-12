127c8228130380eaaa09d73df6d20212f8967703
nluuarch@gmail.com <nluuarch@gmail.com>
2021-12-12 14:38:30 -0800

    Completed Code Along

---
+++ app.rb
@@ -0,0 +1,22 @@
+# puts "Hello world!"
+# puts("Hello world!")
+
+# puts "Hello world!"
+# puts "Hello world!"
+# puts "Hello world!"
+
+# print "Hello world!"
+# print "Hello world!"
+# print "Hello world!"
+
+# p [1,2,3]
+
+# pp [{ id: 1, hello: "World", bye: "Example" }, { id: 2, hello: "Ruby", bye: "example" }, { id: 3, hello: "Moon", bye:"example" }, { id: 4, hello: "Learner", bye: "example" }]
+
+
+puts "Hello World!"
+
+print "Pass this test, please."
+
+p [1,2,3]
+
